039314f6bcdbeb151bd07664390084cce2d58772
Hazem <hazem@islam-mohamed-mac.local>
2019-05-30 14:09:38 -0700

    First Commit

---
+++ MyMovies/MyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Hazem on 5/30/19.
+//  Copyright © 2019 Hazem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
